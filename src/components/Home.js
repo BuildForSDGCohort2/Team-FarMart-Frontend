160b4a7ce8c6ef3017d6232c09750c159df99110
sintimmisa <sintim.misa@gmail.com>
2020-09-03 01:13:50 +0000

    FEATURE: Navbar and Routes created

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Home = () => {
+    return (
+        <div className=''>
+            <h3> Home UI</h3>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const SignUp = () => {
+    return (
+        <div className=''>
+            <h3>SignUp UI</h3>
+        </div>
+    )
+}
+
+export default SignUp;
